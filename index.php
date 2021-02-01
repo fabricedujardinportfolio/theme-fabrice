0bf5410e7807203f24aa0fd6a5043bfe1f695682
Dujardin fabrice <fabricedujardin873@gmail.com>
2021-02-01 21:19:21 +1100

    rajout deux fasont de rajouter du contenue (extrai de la page avec exerpt ligne 24 ou le contenue complet content ligne 23

--- index.php
+++ index.php
@@ -7,17 +7,24 @@
   <link rel="stylesheet" href="<?php echo get_stylesheet_uri();?>">
 </head>
 <body>
-    <main>
-        <h1><?php bloginfo("name");?></h1>
 <!-- Crée une boucle  -->
 <!-- afficher l'ensemble des fichier de la page article -->
-        <?php while(have_posts()) : the_post() ;?><!-- Si il y a des article alors affiche des articles -->
-            <article>
-                <?php the_post_thumbnail("medium"); ?><!-- image mise en avant -->
-                <h1><?php the_title(); ?></h1>
-                <?php the_content(); ?>
-            </article>
-        <?php endwhile;?><!-- Une boucle while se fini toujour par un endwhile -->
-    </main>
+
+<?php while(have_posts()) : the_post() ;?>
+
+<!-- Si il y a des article alors affiche des articles -->
+
+<?php the_post_thumbnail("medium"); ?>
+
+<!-- image mise en avant -->
+
+    <h2><?php the_title(); ?></h2>
+    <!-- Contenue complet de la page  -->
+    <!-- <?php the_content(); ?> -->
+    <?php the_excerpt(); ?><!-- Extrain de la page  -->
+    
+    <?php endwhile;?>
+
+    <!-- Une boucle while se fini toujour par un endwhile -->
 </body>
 </html>
