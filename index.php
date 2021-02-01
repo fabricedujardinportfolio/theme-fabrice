6a32f5a8b3a79e5ecfcc72d6f4bde6f3785e49e7
Dujardin fabrice <fabricedujardin873@gmail.com>
2021-02-01 18:43:29 +1100

    affichage du title en dynamique et du h1 01/02/2021

--- index.php
+++ index.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php bloginfo("name");?> - <?php bloginfo("description");?></title>
+  <link rel="stylesheet" href="<?php echo get_stylesheet_uri();?>">
 </head>
 <body>
-    
+    <h1><?php the_title();?></h1>
 </body>
 </html>
