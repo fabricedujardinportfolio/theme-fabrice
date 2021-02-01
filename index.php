be36aae361d1b95759becb15c95376788ab2cc91
Dujardin fabrice <fabricedujardin873@gmail.com>
2021-02-01 21:02:19 +1100

    rajout de squelette

--- index.php
+++ index.php
@@ -7,12 +7,17 @@
   <link rel="stylesheet" href="<?php echo get_stylesheet_uri();?>">
 </head>
 <body>
+    <main>
+        <h1><?php bloginfo("name");?></h1>
 <!-- Crée une boucle  -->
 <!-- afficher l'ensemble des fichier de la page article -->
-<?php while(have_posts()) : the_post() ;?><!-- Si il y a des article alors affiche des articles -->
-<?php the_post_thumbnail("medium"); ?><!-- image mise en avant -->
-    <h1><?php the_title(); ?></h1>
-    <?php the_content(); ?>
-    <?php endwhile;?><!-- Une boucle while se fini toujour par un endwhile -->
+        <?php while(have_posts()) : the_post() ;?><!-- Si il y a des article alors affiche des articles -->
+            <article>
+                <?php the_post_thumbnail("medium"); ?><!-- image mise en avant -->
+                <h1><?php the_title(); ?></h1>
+                <?php the_content(); ?>
+            </article>
+        <?php endwhile;?><!-- Une boucle while se fini toujour par un endwhile -->
+    </main>
 </body>
 </html>
