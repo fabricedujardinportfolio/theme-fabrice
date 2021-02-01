9eeecf706b77c211239b4239fcaa2b228e9af7de
Dujardin fabrice <fabricedujardin873@gmail.com>
2021-02-01 20:58:01 +1100

    correction de lerreur de l'imort du php UN S EN TROP

--- functions.php
+++ functions.php
@@ -1,2 +1,2 @@
 <?php
-add_theme_support('post-thumbnails'); // Function servant à appeler la mise en avant d'image 
+add_theme_support('post-thumbnails'); // Function servant à appeler la mise en avant de l'image dans un article 
--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 <!-- Crée une boucle  -->
 <!-- afficher l'ensemble des fichier de la page article -->
 <?php while(have_posts()) : the_post() ;?><!-- Si il y a des article alors affiche des articles -->
-<?php the_post_thumbnails();?><!-- image mise en avant -->
+<?php the_post_thumbnail("medium"); ?><!-- image mise en avant -->
     <h1><?php the_title(); ?></h1>
     <?php the_content(); ?>
     <?php endwhile;?><!-- Une boucle while se fini toujour par un endwhile -->
