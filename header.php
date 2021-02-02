b7fa3377db104e6e4ff274d05c47a16eada1c844
Fabrice DUJARDIN <44850811+fabricedujardinportfolio@users.noreply.github.com>
2021-02-03 01:02:22 +1100

    rajout de contenue

--- header.php
+++ header.php
@@ -9,11 +9,20 @@
     </title>
     <!-- Latest compiled and minified CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-<link rel="stylesheet" href="<?php echo get_stylesheet_uri();?>">
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri();?>">
 </head>
 <body>
 <!-- Cette syntaxe force wordpress d'afficher que le menu qui à était cocher sur emplacement "Menu principal des DEV NC" -->
 <!-- Elle ne sera donc pas utiliser sur le menu footer si en voudrais l'utilisé -->
 <!-- <?php wp_nav_menu( array( 'theme_location' => 'menu_principal' ) ); ?> -->
 <!-- MENU -->
-    <?php wp_nav_menu(); ?>
+<nav class="container-fluid">
+    <div class="row bg-secondary">
+        <div class="col-12 col-md-6 col-md-6">    
+            <img src="http://localhost/wordpress/wp-content/uploads/2021/02/logo_open_nc.png" alt="Logo du site" width="auto" height="80px"/>
+        </div>
+        <div class="col-12 col-md-6 col-md-6 m-auto">
+            <?php wp_nav_menu(); ?>
+        </div>
+    </div>
+</nav>    
