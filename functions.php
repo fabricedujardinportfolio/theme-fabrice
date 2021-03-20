ac560489f7c5810968a344bda22def52ee1b62f6
Dujardin fabrice <fabricedujardin873@gmail.com>
2021-03-21 08:47:56 +1100

    correctiion de import de la function post-thumbnails et rajout de la function title-tag

--- functions.php
+++ functions.php
@@ -1,8 +1,12 @@
 <?php
-add_theme_support('post-thumbnails'); // Function servant à appeler la mise en avant de l'image dans un article 
 //Function pour rajouter un menu
 function enregistre_mon_menu() {
     register_nav_menu( 'menu_principal', __( 'Menu principal des DEV NC' ) );
 }
+function montheme_support() {
+add_theme_support('title-tag');
+add_theme_support('post-thumbnails'); // Function servant à appeler la mise en avant de l'image dans un article 
+}
 //Appel de function pour rajouter un menu
 add_action( 'init', 'enregistre_mon_menu' );
+add_action( 'after_setup_theme', 'montheme_support );
