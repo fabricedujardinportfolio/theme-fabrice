a5344c2e2750603b5f9107e3a1714ad2c6653985
Dujardin fabrice <fabricedujardin873@gmail.com>
2021-03-21 08:24:41 +1100

    rajout des appel header et footer de wordpress dans le theme

--- header.php
+++ header.php
@@ -10,6 +10,7 @@
     <!-- Latest compiled and minified CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link rel="stylesheet" href="<?php echo get_stylesheet_uri();?>">
+    <?php wp_head()?>
 </head>
 <body>
 <!-- Cette syntaxe force wordpress d'afficher que le menu qui à était cocher sur emplacement "Menu principal des DEV NC" -->
