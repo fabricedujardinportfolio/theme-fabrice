3408065bda43222bd1e52a204570623fbbac8348
Dujardin fabrice <fabricedujardin873@gmail.com>
2021-03-21 09:41:20 +1100

    rajout de l'ordre de lecture des js , les js en etait rajouter dans le footer plutot que dans le header le jquery de wordpress a etait désactivé pour utilisé notre version de jquery

--- functions.php
+++ functions.php
@@ -12,7 +12,14 @@ function fabrice_support() {
 
 function fabrice_register_asset(){
     wp_register_style('bootstrap','https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css','');//ici en défini un css que l'on peut utilisé 
-    wp_enqueue_style('bootstrap');// Ici en défini le css que l'on vasz utilisé 
+    wp_register_script('bootstrap','https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js',['popper','jquery'],false,true);//ici en défini un js que l'on peut utilisé 
+    wp_register_script('popper','https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js',[],false,true);//ici en défini un js que l'on peut utilisé 
+    wp_deregister_script('jquerymin');//ici nous enlevons ke jquery que wordpress importe pour par la suite importer celuit que nous voulons utilisé
+    wp_register_script('jquery','https://code.jquery.com/jquery-3.2.1.slim.min.js',[],false,true);//ici en défini un js que l'on peut utilisé 
+    wp_enqueue_style('bootstrap');// Ici en défini le css que l'on vas utilisé 
+    wp_enqueue_script('bootstrap');// Ici en défini le js que l'on vas utilisé ce qui est intéréssant à savoir 
+    //ces que nous avons pas besoin de définir les deux autre js car nous les avons déclarés dans le premier 
+    //js importé comme bootstrap dépend de popper et jquery . il le charge donc dans le bonne ordre.
 }
 
 //ACTION
