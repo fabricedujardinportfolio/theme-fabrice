221e90663379961535bdb37501194fd501a7e598
Dujardin fabrice <fabricedujardin873@gmail.com>
2021-03-21 09:46:20 +1100

    mise a niveau du contenue en relation avec le css importter dans function , supression du contenue

--- footer.php
+++ footer.php
@@ -1,10 +1,6 @@
 <footer>
       <p> &#169;FABRICE DUJARDIN DEV WEB 2021</p>
 </footer>
-<!-- jQuery library -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<!-- Latest compiled JavaScript -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
- <?php wp_footer()?>
-  </body>
+<?php wp_footer()?>
+</body>
 </html>
--- header.php
+++ header.php
@@ -7,8 +7,6 @@
         <?php bloginfo("name"); ?> -
         <?php bloginfo("description"); ?>
     </title>
-    <!-- Latest compiled and minified CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link rel="stylesheet" href="<?php echo get_stylesheet_uri();?>">
     <?php wp_head()?>
 </head>
